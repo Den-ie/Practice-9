b2ac76795679f18b84fd60f40e13fa7cd9e2dc6e
Den-ie <dpokosov@gmail.com>
2022-12-14 20:48:19 +0300

    Добавьте файлы проекта.

---
+++ Practice 9/Films.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_9
+{
+    struct Films
+    {
+        public string Film { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public int Duration { get; set; }
+
+        public Films(string film, string genre, int year, int duration)
+        {
+            Film = film;
+            Genre = genre;
+            Year = year;
+            Duration = duration;
+        }
+    }
+}
---
+++ Practice 9/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Practice_9
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Height += 25;
+        }
+
+        List<Films> _movie = new List<Films>();
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(film.Text, out int FILM) & !int.TryParse(genre.Text, out int GENRE) & int.TryParse(year.Text, out int YEAR) & int.TryParse(duration.Text, out int DUR))
+            {
+                _movie.Add(new Films(film.Text, genre.Text, YEAR, DUR));
+                structGrid.ItemsSource = null;
+                structGrid.ItemsSource = _movie;
+            }
+            else MessageBox.Show("Ошибка");
+        }
+
+        private void AboutProgramm(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Заполнить таблицу фильмотеки на 7 кассет с полями: фильм, жанр, год выпуска, \r\nпродолжительность просмотра. Вывести результат на экран. Вывести список \r\nфильмов заданного жанра");
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Genre(object sender, RoutedEventArgs e)
+        {
+            var foundFilms = new StringBuilder();
+
+            foreach (var item in _movie)
+            {
+                if (item.Genre == Janre.Text)
+                {
+                    foundFilms.AppendLine(item.Film);
+                }
+            }
+            MessageBox.Show(foundFilms.ToString());
+        }
+    }
+}
